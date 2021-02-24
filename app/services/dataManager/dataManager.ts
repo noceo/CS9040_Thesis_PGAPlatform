ac5085ae75d220fe050fa0244071fa622a11d711
noceo <paulschade20@gmail.com>
2021-02-24 15:06:26 +0100

    single tree now working in live mode

--- app/services/dataManager/dataManager.ts
+++ app/services/dataManager/dataManager.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'inversify'
+import Vue from 'vue'
 import { IFileParser } from '../fileParserService/FileParser.types'
 import { IParseError } from '../fileParserService/IParseError'
 import { INotificationService } from '../notificationService/notificationService.types'
@@ -10,13 +11,25 @@ import TYPES from '~/IoC/types'
 import { GlobalStoreMutation } from '~/store/modules/global/mutations/mutations.types'
 import { store } from '~/store'
 import { StoreModule } from '~/store/store-modules'
-import { IDataRow } from '~/model/dataRow/dataRow.types'
 import { generateID } from '~/model/helpers/idGenerator'
 import { INumericDataParameter } from '~/model/INumericDataParameter'
 import { IDataParameter } from '~/model/IDataParameter'
 import { IVizParameter } from '~/model/IVizParameter'
 import { ParamMappingType } from '~/model/ParamMappingType'
 import { IMappingFunctionCollection } from '~/model/IMappingFunctionCollection'
+import { GlobalStoreGetter } from '~/store/modules/global/getters/getters.types'
+import { IDataConnection } from '~/model/IDataConnection'
+import { INumericVizParameter } from '~/model/INumericVizParameter'
+import { ITextDataParameter } from '~/model/ITextDataParameter'
+
+interface DataConnections {
+  [key: string]: {
+    id: string
+    inputParam: INumericDataParameter
+    mappingType: ParamMappingType
+    outputParam: INumericVizParameter
+  }
+}
 
 @injectable()
 export class DataManager implements IDataManager {
@@ -24,9 +37,12 @@ export class DataManager implements IDataManager {
   private _notificationService: INotificationService
   private _timer: ITimer
 
-  private _data?: Array<IDataRow>
-  private _currentDataRowIndex: number
+  private _data?: Array<any>
+  private _currentDataIndex: number
+  private _dataParams?: Array<IDataParameter>
   private _mappingFunctions: IMappingFunctionCollection
+  private _dataConnections: DataConnections
+  private _updateFunction: () => void
 
   constructor(
     @inject(TYPES.IFileParser) parser: IFileParser<object>,
@@ -37,8 +53,19 @@ export class DataManager implements IDataManager {
     this._parser = parser
     this._notificationService = notificationsService
     this._timer = timer
-    this._currentDataRowIndex = 0
+    // this._currentDataRowIndex = 0
     this._mappingFunctions = mappingFunctions
+    const functionTypes = []
+    for (const element of Object.keys(this._mappingFunctions)) {
+      functionTypes.push(element)
+    }
+    store.commit(
+      `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_MAPPING_FUNCTIONS}`,
+      functionTypes
+    )
+    this._currentDataIndex = 0
+    this._dataConnections = {}
+    this._updateFunction = this.writeUpdatedValuesToStore.bind(this)
     this.addMutationHandlers()
   }
 
@@ -51,22 +78,9 @@ export class DataManager implements IDataManager {
         })
 
         if (result.meta.fields) {
-          // Write to vuex ...
-          const dataParams: Array<INumericDataParameter> = []
-          result.meta.fields?.forEach((element) => {
-            dataParams.push({
-              id: generateID(),
-              name: element,
-              min: 0,
-              max: 0,
-              value: 0,
-              dataConnectionId: '',
-            })
-          })
-          store.commit(
-            `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_DATA_PARAMS_NUMERIC}`,
-            dataParams
-          )
+          this.createDataParams(result.meta.fields)
+          this._data = result.data
+          console.warn(this._dataParams)
           resolve({ result, file })
         }
         reject(new Error('Data Fields not readable'))
@@ -90,19 +104,22 @@ export class DataManager implements IDataManager {
   }
 
   startDataPropagation(): void {
+    console.log('VIZ_START')
     store.commit(
       `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_VISUALIZATION_ACTIVE}`,
       true
     )
-    this._timer.on('tick', this.writeNewDataRowToStore)
+    this._timer.on('tick', this._updateFunction)
     this._timer.start()
   }
 
   stopDataPropagation(): void {
+    console.log('VIZ_STOP')
     store.commit(
       `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_VISUALIZATION_ACTIVE}`,
       false
     )
+    this._timer.off('tick', this._updateFunction)
     this._timer.stop()
   }
 
@@ -127,34 +144,161 @@ export class DataManager implements IDataManager {
     )
   }
 
+  private createDataParams(fileHeaders: Array<string>): void {
+    const numericDataParams: Array<INumericDataParameter> = []
+    const textDataParams: Array<ITextDataParameter> = []
+    console.log(fileHeaders)
+    const headerStringRegex = /^.*_(string)$/
+    const headerNumberRegex = /^.*_(number)_(0|-?[1-9][0-9]{0,3})_(0|-?[1-9][0-9]{0,3})$/
+    for (const header of fileHeaders) {
+      const headerStringArray = header.split('_')
+      const paramName = headerStringArray[0]
+      if (headerStringRegex.test(header)) {
+        textDataParams.push({
+          id: generateID(),
+          name: paramName,
+          value: '',
+          dataConnectionId: '',
+        })
+      } else if (headerNumberRegex.test(header)) {
+        const min = parseInt(headerStringArray[2])
+        const max = parseInt(headerStringArray[3])
+        numericDataParams.push({
+          id: generateID(),
+          name: paramName,
+          min,
+          max,
+          value: 0,
+          dataConnectionId: '',
+        })
+      }
+    }
+    store.commit(
+      `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_DATA_PARAMS_NUMERIC}`,
+      numericDataParams
+    )
+    store.commit(
+      `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_DATA_PARAMS_TEXT}`,
+      textDataParams
+    )
+    store.commit(
+      `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_FILE_STATE}`,
+      true
+    )
+    this._dataParams = [...numericDataParams, ...textDataParams]
+  }
+
   private addMutationHandlers(): void {
-    store.subscribe((mutation: any) => {
+    store.subscribe((mutation: { type: string; payload: IDataConnection }) => {
       switch (mutation.type) {
         case `${StoreModule.GLOBAL}/${GlobalStoreMutation.ADD_DATA_CONNECTION}`: {
-          console.log('ADD_CONNECTION', mutation)
-          break
-        }
-        case `${StoreModule.GLOBAL}/${GlobalStoreMutation.UPDATE_DATA_CONNECTION}`: {
-          console.log('UPDATE_CONNECTION', mutation)
+          const dataParam = store.getters[
+            `${StoreModule.GLOBAL}/${GlobalStoreGetter.GET_DATA_PARAM_NUMERIC_BY_ID}`
+          ](mutation.payload.dataParamId)
+          const vizParam = store.getters[
+            `${StoreModule.GLOBAL}/${GlobalStoreGetter.GET_VIZ_PARAM_NUMERIC_BY_ID}`
+          ](mutation.payload.vizParamId)
+          console.log('VIZ_PARAM', vizParam, mutation.payload.vizParamId)
+          const newDataConnection = {
+            id: mutation.payload.id,
+            inputParam: dataParam,
+            mappingType: mutation.payload.mappingType,
+            outputParam: vizParam,
+          }
+          this._dataConnections[mutation.payload.id] = newDataConnection
+          console.log('ADD', this._dataConnections)
           break
         }
         case `${StoreModule.GLOBAL}/${GlobalStoreMutation.REMOVE_DATA_CONNECTION}`: {
           console.log('REMOVE_CONNECTION', mutation)
+          const foundConnectionId = Object.keys(this._dataConnections).find(
+            (connectionId) => connectionId === mutation.payload.id
+          )
+          if (foundConnectionId) {
+            delete this._dataConnections[foundConnectionId]
+          }
           break
         }
       }
     })
   }
 
-  private writeNewDataRowToStore(): void {
-    if (typeof this._data !== 'undefined') {
-      console.log('CUR_DATA_ROW', this._data[this._currentDataRowIndex])
-      store.commit(
-        `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_CURRENT_DATAROW}`,
-        this._data[this._currentDataRowIndex]
-      )
-      this._currentDataRowIndex++
+  private getInitialDataConnections(): void {}
+
+  private writeUpdatedValuesToStore(): void {
+    console.log('wrote new data to store', this._dataConnections)
+    if (!this._data) {
+      return
     }
+    // set new dataParamValues
+    console.log(this._data[this._currentDataIndex])
+    const currentDataRow = this._data[this._currentDataIndex]
+    const numberRegex = /^((0[.|,]\d*)|(([1-9]{1}\d*[.|,]\d*))|(0|[1-9]*))$/
+    for (const dataParam of Object.entries(currentDataRow)) {
+      if (dataParam[1] !== '-' && dataParam[1] !== '') {
+        const paramName = dataParam[0].split('_')[0]
+        const paramValue = dataParam[1] as string
+        let parsedValue: any
+        if (numberRegex.test(dataParam[1] as string)) {
+          parsedValue = parseFloat(paramValue.replace(',', '.'))
+          store.commit(
+            `${StoreModule.GLOBAL}/${GlobalStoreMutation.UPDATE_DATA_PARAM_NUMERIC_BY_NAME}`,
+            { name: paramName, value: parsedValue }
+          )
+        } else {
+          parsedValue = paramValue
+          store.commit(
+            `${StoreModule.GLOBAL}/${GlobalStoreMutation.UPDATE_DATA_PARAM_TEXT_BY_NAME}`,
+            { name: paramName, value: parsedValue }
+          )
+        }
+        console.log('updated', paramName, parsedValue)
+      }
+    }
+    if (this._currentDataIndex > this._data.length - 1) {
+      this._currentDataIndex = 0
+    } else {
+      this._currentDataIndex++
+    }
+
+    // update all mapped vizParams
+    for (const dataConnection of Object.values(this._dataConnections)) {
+      console.log(dataConnection)
+      const inputParam = dataConnection.inputParam
+      const mappingFunction = this._mappingFunctions[dataConnection.mappingType]
+      const outputParam = dataConnection.outputParam
+      const newVizParamValue = mappingFunction(
+        inputParam.value,
+        inputParam.min,
+        inputParam.max,
+        outputParam.min,
+        outputParam.max
+      )
+      const newVizParam: INumericVizParameter = {
+        id: outputParam.id,
+        name: outputParam.name,
+        value: newVizParamValue,
+        min: outputParam.min,
+        max: outputParam.max,
+        valueModifier: outputParam.valueModifier,
+        dataConnectionId: dataConnection.id,
+      }
+      store.commit(
+        `${StoreModule.GLOBAL}/${GlobalStoreMutation.UPDATE_VIZ_PARAM_NUMERIC}`,
+        newVizParam
+      )
+    }
+    console.log('ALL PARAMS CHANGED')
+    store.commit(
+      `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_DATA_TRANSFER_STATE}`,
+      true
+    )
+    Vue.nextTick(() => {
+      store.commit(
+        `${StoreModule.GLOBAL}/${GlobalStoreMutation.SET_DATA_TRANSFER_STATE}`,
+        false
+      )
+    })
   }
 
   private writeAllDataToStore(): void {}
